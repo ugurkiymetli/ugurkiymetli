e4efb5a91e7bfd10e21d59727226c192efccd71b
uğur kıymetli <ugurkiymetli@hotmail.com>
2022-11-22 00:50:16 +0300

    ugurkiymetli.com v2 updated

---
+++ precache-manifest.78e61cbd185289c6a56c2d5feff4e459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a30c9c7b03477b45d20",
+    "url": "/static/css/main.333c2317.chunk.css"
+  },
+  {
+    "revision": "3a30c9c7b03477b45d20",
+    "url": "/static/js/main.3a30c9c7.chunk.js"
+  },
+  {
+    "revision": "cdea558ebfb418344ea6",
+    "url": "/static/js/1.cdea558e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2975eaad4f8b503a7df20623a1e941d0",
+    "url": "/index.html"
+  }
+];

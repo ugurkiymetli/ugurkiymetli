9dc0521743e4430292a4cde233c8d93a37530ae1
uğur kıymetli <ugurkiymetli@hotmail.com>
2023-06-28 23:50:50 +0300

    manual build

---
+++ precache-manifest.257a107aa62accccbec344247386604c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dda3d52fafe6b6bd9348",
+    "url": "/static/css/main.333c2317.chunk.css"
+  },
+  {
+    "revision": "dda3d52fafe6b6bd9348",
+    "url": "/static/js/main.dda3d52f.chunk.js"
+  },
+  {
+    "revision": "cdea558ebfb418344ea6",
+    "url": "/static/js/1.cdea558e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4847fc1c911e5d2e039b43aa87b5d591",
+    "url": "/index.html"
+  }
+];
